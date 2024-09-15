695d88a1718610a30b365bc75ed07459b93348cd
Andre Louis Issa <andrelouisissa@gmail.com>
2024-09-16 02:20:37 +1000

    remap _PLUGIN

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,13 @@
 ---@meta _
 ---@diagnostic disable
 
-local rom = _G
+local rom = rom or _G
+local _PLUGIN = _PLUGIN or {}
+for k,v in pairs(_G) do
+	if type(k) == 'string' and k:sub(1,1) == '!' then
+		_PLUGIN[k] = v
+	end
+end
 
 ---@module 'MGReturns-ENVY'
 local envy = rom.mods["MGReturns-ENVY"]
@@ -9,6 +15,7 @@ local envy = rom.mods["MGReturns-ENVY"]
 envy.auto()
 private.envy = envy
 private.rom = rom
+private._PLUGIN = _PLUGIN
 
 import('core.lua')
 import('browser.lua',nil,envy.globals)
